679e2b26d7d1434595462462b0dac63e790a1cdd
Jess Szmajda <jess@optoro.com>
2017-12-18 20:12:04 -0500

    validating structure of publisher

--- lib/avro_pinions/configuration.rb
+++ lib/avro_pinions/configuration.rb
@@ -8,5 +8,15 @@ module AvroPinions
 
     def initialize
     end
+
+    def publisher=(pub)
+      unless pub.respond_to?(:publish) && pub.method(:publish).arity == 2
+        raise InvalidPublisher.new("Please define a publisher that responds to publish and receives a topic and message")
+      end
+      @publisher = pub
+    end
+
+    class InvalidPublisher < StandardError
+    end
   end
 end
--- spec/avro_pinions/configuration_spec.rb
+++ spec/avro_pinions/configuration_spec.rb
@@ -1,14 +1,36 @@
 require 'spec_helper'
 
 describe AvroPinions::Configuration do
-  it "accepts a publishing target" do
-    stuff = Class.new do
-      def publish(topic, message)
-        42
-      end
+  class InvalidPublisher
+  end
+  class InvalidPublisher2
+    def publish
     end
-    AvroPinions.configure(publisher: stuff.new)
+  end
+  class ValidPublisher
+    def publish(topic, message)
+      # yep that's it
+      42
+    end
+  end
+  describe "publisher" do
+    it "accepts a publishing target" do
+      stuff = ValidPublisher.new
+      AvroPinions.configure(publisher: stuff)
 
-    expect(AvroPinions.configuration.publisher.publish('stuff', 'things')).to eq(42)
+      expect(AvroPinions.configuration.publisher.publish('stuff', 'things')).to eq(42)
+    end
+
+    it "validates the publisher against some expected APIs" do
+      expect {
+        AvroPinions.configure(publisher: InvalidPublisher.new)
+      }.to raise_exception(AvroPinions::Configuration::InvalidPublisher)
+      expect {
+        AvroPinions.configure(publisher: InvalidPublisher2.new)
+      }.to raise_exception(AvroPinions::Configuration::InvalidPublisher)
+      expect {
+        AvroPinions.configure(publisher: ValidPublisher.new)
+      }.to_not raise_error
+    end
   end
 end
